8ed09c204162591e0731573f532c1500b6fcfc28
viguu007 <78961208+viguu007@users.noreply.github.com>
2021-03-11 13:02:45 +0530

    Update mis.js

--- mis.js
+++ mis.js
@@ -12,7 +12,7 @@ function agencies()
 		document.getElementById("a1").innerHTML="<a id=\"t\" href=\"isromissions.html\" target=\"Space\">ISRO</a>";
 		document.getElementById("a2").innerHTML="<a id=\"t\" href=\"nasamissions.html\" target=\"Space\">NASA</a>";
 		document.getElementById("a3").innerHTML="<a id=\"t\" href=\"sspmissions.html\" target=\"Space\">INTERKOSMOS</a>";
-		document.getElementById("a1").style.backgroundColor="yellow";
-		document.getElementById("a2").style.backgroundColor="yellow";
-		document.getElementById("a3").style.backgroundColor="yellow";
+		document.getElementById("a1").style.backgroundColor="#a39b9b";
+		document.getElementById("a2").style.backgroundColor="#a39b9b";
+		document.getElementById("a3").style.backgroundColor="#a39b9b";
 	}
